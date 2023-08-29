cf46e6fe109650b8643ce352b079852b5fbea460
vinodpkd <vinodxx@gmail.com>
2023-08-29 20:42:22 +0700

    Update nsmallestlargest.jl

--- nsmallestlargest.jl
+++ nsmallestlargest.jl
@@ -10,3 +10,15 @@ portfolio = [
 N = 3
 sort(portfolio, by = x -> x["price"])[1:N] #N smallest shares by price
 sort(portfolio, by = x -> x["price"],rev=true)[1:N] #N largest by price
+
+
+portfolio = [
+          (name= "IBM", shares= 100, price= 91.2),
+          (name= "AAPL", shares= 50, price= 643.22),
+          (name= "FB", shares= 150, price= 21.09),
+          (name= "HPQ", shares= 35, price= 31.75),
+          (name= "YHOO", shares= 45, price= 16.35),
+          (name= "ACME", shares= 75, price= 115.65)
+       ]
+
+sort(portfolio, by = row->row.price)[1:N]
